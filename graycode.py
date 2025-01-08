3cbb3eb4db6a4bcee9b270fa30be3e71778d5625
Aymen <aymenbasilicum@gmail.com>
2025-01-08 22:36:54 +0100

    Create graycode.py

---
+++ graycode.py
@@ -0,0 +1,18 @@
+def gray(number):
+    code = number[0]
+    for b in range(1, len(number)):
+        code += str(int(number[b])^int(number[b-1])) # XOR between the current and previous bits
+    return code
+
+number = input("Enter your number: ")
+choice = input("Is your number expressed in the binary numeral system? (y/n) ").lower()
+
+if choice == 'n':
+    if number.isdigit():
+        number = format(int(number), '04b') # converts to binary 
+
+    else:
+        print("Enter a valid decimal number.")
+        exit()
+
+print(f"({gray(number)})GRAY")
